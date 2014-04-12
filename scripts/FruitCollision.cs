6812b6a9f261dbdb7c06e5b35088f1e7d43db501
Pridexs <alehstk@gmail.com>
2014-04-12 18:05:16 -0300

    Update FruitCollision.cs

--- scripts/FruitCollision.cs
+++ scripts/FruitCollision.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class FruitCollision : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	void OnTriggerEnter (Collider col) {
 		SnakeHead script = ((SnakeHead)col.gameObject.GetComponent (typeof(SnakeHead)));
 		if(script != null) {
